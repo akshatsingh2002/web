663a6e29aabd0e52010868384f5bce28a946f48d
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-07-14 14:28:00 +0530

    commit

--- calculator.js
+++ calculator.js
@@ -1,3 +1,9 @@
-function func1(val){
-    document.getElementById("text").value=+val;
+function display(val){
+    document.getElementById("text").value+=val;
+}
+function solve(){
+    n=document.getElementById("text").value;
+    // console.log(n);
+    let y=eval(n);
+    document.getElementById("text").value=y;
 }
--- digitalclock.js
+++ digitalclock.js
@@ -4,6 +4,9 @@ function calc(){
     let m=y.getMinutes();
     let s=y.getSeconds();
     let zone="AM";
+    if(h>=12){
+        zone="PM";
+    }
     if(s<10){
     s="0" +s;    
     }
@@ -11,24 +14,31 @@ function calc(){
         m="0"+m;    
         }
         if(h>12){
-            zone="PM"
             h=h-12;
         }
         if(h<10){
             h="0"+h;
         }
     let str=h+":"+m+":"+s+" "+zone;
-    document.getElementById("digital").innerText=str;
+    document.getElementById("digital").innerHTML=str;
     const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 let name = month[y.getMonth()];
 let d=y.getUTCDate();
 let yer=y.getUTCFullYear();
 let cad=d+" "+name +""+yer;
 document.getElementById("calander").innerHTML=cad;
+function img(){
+    if(zone=="AM" && h>=4 && h<12)
+    document.getElementById("background").style.backgroundImage="url('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiKmQnzF2P7fUguKjTUTh5RLwKSzj02f7eIv2eCAiWIB29QQLXFTiaYYRKS7LMQCCn39tPINH3CydmUm_0-bppa5xE4MemtHrb6kVT8BM3f6iqS5g4kpsQTsBTLUYLbWILJnZp2CpYXBQWkZmJ2UEYyrshpkmc9GQOfOy7iQyQrsF59NP1rN2yPQl7T/s1014/IMG_20220320_154902.jpg')";
+   else if(zone=="PM"&&h==12||h<5)   
+    document.getElementById("background").style.backgroundImage="url('https://d1csarkz8obe9u.cloudfront.net/posterpreviews/good-afternoon-instagram-post-design-template-ec37d94e2e9449fc0f44db2df658c14a_screen.jpg?ts=1640726752')";
+    else if(zone=="PM"&&h>=5&&h<8)  
+    document.getElementById("background").style.backgroundImage="url('https://www.wishesmsg.com/wp-content/uploads/Good-Evening.jpg')";
+    else
+    document.getElementById("background").style.backgroundImage="url('https://muskaanhindi.com/wp-content/uploads/2022/01/beautiful-good-night-images-hd.jpg')";
+   }
+img()
     var t=setTimeout(function(){calc()},1000);
-    function img(){
-    document.getElementById("bg").style.backgroundImage="url('https://mustsharenews.com/wp-content/uploads/2022/05/sleepy-crayon-shin-chan-mouse-1.jpg')";
-    }
-    img()
+    
 }
 calc()
