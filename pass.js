4dcd521ab868d6e1bbcc5339d49278838ae2f11c
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-07-19 16:27:21 +0530

    commit

--- pass.js
+++ pass.js
@@ -1,11 +1,21 @@
 function input(){
  mail=document.getElementById("ml").value;
+ for(i=0;i<mail.length;i++){
+  if(mail[i]=="@"){
+  document.getElementById("ml").value=mail+"gmail.com"
+  mail=mail+"gmail.com"
+  break
+  }
+ }
     if(mail.endsWith("@gmail.com")){
+      document.getElementById("mailbox").innerText="email verified"
+      // return false
+    }
+     else{ 
+
         document.getElementById("mailbox").innerText="invalid email only use gmail id"
         console.log('@ is not allowed')
-        return false
-    }
-    else 
-    document.getElementById("mailbox").innerText=""
-  }
+        // return false
+     }
   
+    }
