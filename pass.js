c58f81f26383edb35c7b051b318047d3b11e3d39
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-07-20 12:28:08 +0530

    commit

--- pass.js
+++ pass.js
@@ -1,21 +1,50 @@
 function input(){
  mail=document.getElementById("ml").value;
- for(i=0;i<mail.length;i++){
-  if(mail[i]=="@"){
-  document.getElementById("ml").value=mail+"gmail.com"
-  mail=mail+"gmail.com"
-  break
+ con  = 0
+  l = mail.length
+  for(i=0;i<l;i++){
+   if(mail[i]!="@")
+   con = 1 
+   else {
+   con = 0 
+   break
+   }
   }
- }
-    if(mail.endsWith("@gmail.com")){
-      document.getElementById("mailbox").innerText="email verified"
-      // return false
+  if (con == 1 ){
+   mail=mail+"@gmail.com"
+   document.getElementById("ml").value=mail
+  }
+  if (mail.endsWith("@gmail.com")){
+   document.getElementById("mailbox").innerText="email verified"
+  console.log("yes")
+  } 
+   else{
+   document.getElementById("mailbox").innerText="invalid email"
+   console.log("no")
+   }
     }
-     else{ 
 
-        document.getElementById("mailbox").innerText="invalid email only use gmail id"
-        console.log('@ is not allowed')
-        // return false
-     }
-  
-    }
+function checkpass1(){
+   fname = document.getElementById("first_name").value
+   lname = document.getElementById("last_name").value
+   uname = document.getElementById("uname").value
+   console.log(fname,lname,uname)
+}
+
+function checkpass(){
+   var u=/[A-Z]/g
+   var lu=/[a-z]/g
+   var nu=/[0-9]/g
+   // var s =['!','@','*','.']
+   passowrd=document.getElementById("pass").value
+   console.log(passowrd)
+   if(passowrd.length<8 )
+   document.getElementById("pass_m").innerHTML="password too short shoud be at least 8 characters"
+   else if (passowrd.match(fname) || passowrd.match(lname) || passowrd.match(uname))
+   document.getElementById("pass_m").innerHTML="cannot create password on the input credentials"
+   else if ( passowrd.match(u) && passowrd.match(lu) && passowrd.match(nu) &&(passowrd.match('#') ||passowrd.match('.')||passowrd.match('*')||passowrd.match('@')) ){
+      document.getElementById("pass_m").innerHTML="valid password"
+   }
+   else
+   document.getElementById("pass_m").innerHTML="invalid password"
+}
