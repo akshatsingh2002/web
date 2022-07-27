4c07847a4bf5dc7078399001c242b949a7f40798
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-07-27 19:43:40 +0530

    commit

--- count.js
+++ count.js
@@ -1,7 +1,14 @@
 function count(){
     n = document.getElementById("text_area").value
     val = document.getElementById("value").value
-    for( i=val.length;i>=0;i--)
-    val2
+    splitstring = val.split("")
+    r_string = splitstring.reverse()
+    j_string = r_string.join("")
+    console.log(j_string)
     console.log(val)
+    console.log(n)
+    arr= n.match(val)
+    arr2 = n.match(j_string)
+    document.getElementById("final_cout").innerHTML=arr.length + arr2.length
+   
 }
