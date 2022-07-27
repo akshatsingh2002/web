8555769022e1185ce79f898897d094a97336927d
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-07-27 12:36:52 +0530

    commit

---
+++ count.js
@@ -0,0 +1,7 @@
+function count(){
+    n = document.getElementById("text_area").value
+    val = document.getElementById("value").value
+    for( i=val.length;i>=0;i--)
+    val2
+    console.log(val)
+}
