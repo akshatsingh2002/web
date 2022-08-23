703c51f827f209f0da3dd2628ca3a382c4afd983
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-08-23 16:23:14 +0530

    commit

---
+++ integrated_project.js
@@ -0,0 +1,7 @@
+// ReffradId: IIPRSS1828
+// CollegeName:Jecrc
+// Subject:PhthonDjango
+function popup(){
+    alert("form has been submitted");
+    document.getElementById("span_alert").innerHTML="form has been submitted";
+}
