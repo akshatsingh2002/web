69d7c7e44e322f049b74f639582b5b267dca2153
akshatsingh2002 <68433057+akshatsingh2002@users.noreply.github.com>
2022-10-05 22:18:10 +0530

    commit

---
+++ assignment1.js
@@ -0,0 +1,7 @@
+function calculate() {
+        let var1 = Number(document.querySelector("text area"[0]).value)
+        let var2 =Number(document.querySelector("text area"[]).value)
+            console.log('hello')
+            document.querySelector("p").innerHTML = var1 + var2
+
+}
